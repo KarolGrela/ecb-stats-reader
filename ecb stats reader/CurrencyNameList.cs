e61b11c6e23aa1e3a64baff4a6fd4f9ef8789c98
karol.grela <karol.grela@vp.pl>
2021-03-09 18:11:18 +0100

    CurrencyNameList.Generate() opens the .csv file only once per runtime

--- ecb stats reader/CurrencyNameList.cs
+++ ecb stats reader/CurrencyNameList.cs
@@ -23,25 +23,30 @@ namespace ecb_stats_reader
     {
         private const string path = @"../../../ecb stats reader/currencies.csv";    // path to .csv file
         static private List<CurrencyName> names;                                    // list of CurrencyName instances
+        static private bool isGenerated = false;                                    // true if list has been created
 
         /// <summary>
         /// Static function saving data from .csv file to List
         /// </summary>
         static public void Generate()
         {
-            // read .csv file
-            // output is saved to array of strings, where each line is a string
-            // easch line consist of abbreviation and name of currency separated with a semicolon (;)
-            // e.g. CRR;Currency Name
-            string[] lines = System.IO.File.ReadAllLines(path);     // read .csv file and save it to array of strings     
-            names = new List<CurrencyName>();                       // initialize final List
-            foreach (string line in lines)                          // Loop through "lines" array
-            {
-                string[] words = line.Split(';');                   // split a given line into two strings, split on semicolon; semicolon itself is deleted             
-                CurrencyName name = new CurrencyName();             // create a new instance of "CurrencyName" object
-                name.abbreviation = words[0];                       // save abbreviation to the new instance of "CurrencyName" object
-                name.name = words[1];                               // save name to the new instance of "CurrencyName" object
-                names.Add(name);                                    // add this instance of "CurrencyName" to the list
+            if (isGenerated == false) // if list hasn't benn created yet
+            { 
+                // read .csv file
+                // output is saved to array of strings, where each line is a string
+                // easch line consist of abbreviation and name of currency separated with a semicolon (;)
+                // e.g. CRR;Currency Name
+                string[] lines = System.IO.File.ReadAllLines(path);     // read .csv file and save it to array of strings     
+                names = new List<CurrencyName>();                       // initialize final List
+                foreach (string line in lines)                          // Loop through "lines" array
+                {
+                    string[] words = line.Split(';');                   // split a given line into two strings, split on semicolon; semicolon itself is deleted             
+                    CurrencyName name = new CurrencyName();             // create a new instance of "CurrencyName" object
+                    name.abbreviation = words[0];                       // save abbreviation to the new instance of "CurrencyName" object
+                    name.name = words[1];                               // save name to the new instance of "CurrencyName" object
+                    names.Add(name);                                    // add this instance of "CurrencyName" to the list
+                }
+                isGenerated = true;                                     // save memory
             }
         }
 
